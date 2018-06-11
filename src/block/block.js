2cd6dc1cc8ca310df5014b5eaacd8f0cdd93cb74
laras126 <laras126@gmail.com>
2018-06-11 15:34:16 +0200

    Add RichText

--- src/block/block.js
+++ src/block/block.js
@@ -9,85 +9,56 @@
 import './style.scss';
 import './editor.scss';
 
-const { __ } = wp.i18n; // Import __() from wp.i18n
-const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.blocks
+const { __ } = wp.i18n;
+const { registerBlockType } = wp.blocks;
+const { RichText } = wp.editor; // Import the RichText component
 
-/**
- * Register: aa Gutenberg Block.
- *
- * Registers a new block provided a unique name and an object defining its
- * behavior. Once registered, the block is made editor as an option to any
- * editor interface where blocks are implemented.
- *
- * @link https://wordpress.org/gutenberg/handbook/block-api/
- * @param  {string}   name     Block name.
- * @param  {Object}   settings Block settings.
- * @return {?WPBlock}          The block, if it has been successfully
- *                             registered; otherwise `undefined`.
- */
-registerBlockType( 'cgb/block-wceu-block', {
-	// Block name. Block names must be string that contains a namespace prefix. Example: my-plugin/my-custom-block.
-	title: __( 'wceu-block - CGB Block' ), // Block title.
-	icon: 'shield', // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
-	category: 'common', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
+registerBlockType('cgb/block-wceu-block', {
+	title: __('WCEU Block'),
+	icon: 'carrot',
+	category: 'common',
 	keywords: [
-		__( 'wceu-block — CGB Block' ),
-		__( 'CGB Example' ),
-		__( 'create-guten-block' ),
+		__('wceu-block — CGB Block'),
+		__('CGB Example'),
+		__('create-guten-block'),
 	],
+	attributes: {
+		// Register a "content" attribute so we can track its state.
+		content: {
+			type: 'array',
+			source: 'children',
+			selector: 'p'
+		}
+	},
 
-	/**
-	 * The edit function describes the structure of your block in the context of the editor.
-	 * This represents what the editor will render when the block is used.
-	 *
-	 * The "edit" property must be a valid function.
-	 *
-	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
-	 */
-	edit: function( props ) {
-		// Creates a <p class='wp-block-cgb-block-wceu-block'></p>.
+	edit: function (props) {
+
+		// A callback function to update the block attributes.
+		const onChangeContent = newContent => {
+			props.setAttributes({ content: newContent });
+		};
+
 		return (
-			<div className={ props.className }>
-				<p>— Hello from the backend.</p>
-				<p>
-					CGB BLOCK: <code>wceu-block</code> is a new Gutenberg block
-				</p>
-				<p>
-					It was created via{ ' ' }
-					<code>
-						<a href="https://github.com/ahmadawais/create-guten-block">
-							create-guten-block
-						</a>
-					</code>.
-				</p>
+			// A div wraps the block and uses the class name generated from the block.
+			<div className={props.className}>
+				<RichText
+					onChange={onChangeContent}
+					value={props.attributes.content}
+				/>
 			</div>
 		);
 	},
 
-	/**
-	 * The save function defines the way in which the different attributes should be combined
-	 * into the final markup, which is then serialized by Gutenberg into post_content.
-	 *
-	 * The "save" property must be specified and must be a valid function.
-	 *
-	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
-	 */
-	save: function( props ) {
+	save: function (props) {
 		return (
-			<div>
-				<p>— Hello from the frontend.</p>
-				<p>
-					CGB BLOCK: <code>wceu-block</code> is a new Gutenberg block.
-				</p>
-				<p>
-					It was created via{ ' ' }
-					<code>
-						<a href="https://github.com/ahmadawais/create-guten-block">
-							create-guten-block
-						</a>
-					</code>.
-				</p>
+			// Apply the same wrapper and class name to the saved version of the block.
+			<div className={props.className}>
+				<p>{props.attributes.content}</p>
+				{/* 
+					This can also be done in the same fashion as the core blocks using the RichText component: 
+					<RichText.Content value={ props.attributes.content } />
+				*/}
 			</div>
 		);
 	},
-} );
+});
